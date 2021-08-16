c5249dfcd122adb97f87436f7f8eff490cf0bbf5
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-08-16 12:05:38 +0300

    Check api work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React, {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/country')
+        .then(response => response.json())
+        .then(responseBody => console.log(responseBody))
+        .catch(reason => console.log(reason))
+  }, [])
   return (
     <div>
 
